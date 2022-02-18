db587fea530de084dd853aa22fde3f855885fdf8
mikkotampio <tampio.mikko@gmail.com>
2022-02-18 18:57:04 +0200

    added rss

--- publish.sh
+++ publish.sh
@@ -3,7 +3,8 @@
 shopt -s globstar
 set -e
 
-beagic --module-path beagic new beagic/new/new_content.bgc beagic/new/new_content.html
+beagic --module-path beagic new beagic/new_content.bgc beagic/new/new_content.html
+beagic --module-path beagic rss beagic/new_content.bgc html/rss.xml
 
 for PAGE in pages/**/*.bgc; do
     echo "Compiling $PAGE..."
